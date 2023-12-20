f3a051725731d2f212d12eefd2ced0f02e619d73
EunhoKang <zay0njugin@gmail.com>
2023-12-20 23:28:15 +0900

    edit data type

--- app/src/main/java/com/example/twizydeliveryapp/DriverStatusViewModel.kt
+++ app/src/main/java/com/example/twizydeliveryapp/DriverStatusViewModel.kt
@@ -6,11 +6,11 @@ import kotlinx.coroutines.flow.StateFlow
 
 class DriverStatusViewModel : ViewModel() {
     data class DriverData(
-        val rr: Int = 0, //분당 호흡수
-        val bpm: Int = 0, //분당 심박수
+        val rr: List<Int> = listOf(), //분당 호흡수
+        val bpm: List<Int> = listOf(), //분당 심박수
         val heartRateLevel: Int = 0, //심박 수준
         val respiratoryLevel: Int = 0, //호흡 수준
-        val movementScore: Int = 0
+        val movementScore: Int = 100
     )
 
     private var _currentState = MutableStateFlow(DriverData())
